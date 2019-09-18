7ef479dab2f80928f8ad46db5c0d94d76b27d688
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-09-19 00:20:41 +0900

    インデントを整える (E128)

--- oursite/tresure/utility.py
+++ oursite/tresure/utility.py
@@ -5,11 +5,15 @@ class ConversionTableResolver:
     def createTable(difficulty_pk):
         conversionTable = ConversionTable()
         for i in range(0b1_0000_0000):
-            conversionTable.data.append({
-                'binary': format(i, '08b'),
-                'to_base':
-                    (format(i, '08d') if difficulty_pk == 2
-                    else format(i, '08x'))})
+            conversionTable.data.append(
+                {
+                    'binary': format(i, '08b'),
+                    'to_base': (
+                        format(i, '08d') if difficulty_pk == 2
+                        else format(i, '08x')
+                    )
+                }
+            )
         return conversionTable
 
 
